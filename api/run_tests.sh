2339c55292a95479f32b1753c086928930d9da44
Rafał Łazicki <rafal.lazicki99@gmail.com>
2022-08-23 23:19:19 +0200

    [IMP] Set up postgresql locally and for tests

---
+++ api/run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "Waiting for postgres..."
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py migrate
+pytest -s
